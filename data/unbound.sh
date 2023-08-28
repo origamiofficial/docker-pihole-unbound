96941cf04cc4f43d517e8a25554b125485bc6134
Origami Official <64251776+origamiofficial@users.noreply.github.com>
2023-08-28 18:18:04 +0600

    Update "root.hints" file
    Co-Authored-By: chriexpe <16724821+chriexpe@users.noreply.github.com>

--- data/unbound.sh
+++ data/unbound.sh
@@ -364,5 +364,6 @@ cp -a /dev/random /dev/urandom /dev/null /opt/unbound/etc/unbound/dev/
 mkdir -p -m 700 /opt/unbound/etc/unbound/var && \
 chown _unbound:_unbound /opt/unbound/etc/unbound/var && \
 /opt/unbound/sbin/unbound-anchor -a /opt/unbound/etc/unbound/var/root.key
+curl https://www.internic.net/domain/named.root -q -o /usr/share/dns/root.hints
 
 exec /opt/unbound/sbin/unbound -d -c /opt/unbound/etc/unbound/unbound.conf
